0655405409000432d8ae77837a51a88a744ee23d
Peter Nowee <peter@peternowee.com>
2023-07-06 17:23:26 +0200

    Basic setup for individual-products page

--- app/controllers/individual-products.js
+++ app/controllers/individual-products.js
@@ -1,3 +1,31 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
-export default class IndividualProductsController extends Controller {}
+export default class IndividualProductsController extends Controller {
+  @tracked newSerialNumber = '';
+  @tracked newName = '';
+
+  @service store;
+
+  @action
+  createIndividualProduct(event) {
+    event.preventDefault();
+    // create the new individualProduct
+    const individualProduct = this.store.createRecord('individual-product', {
+      serialNumber: this.newSerialNumber,
+      name: this.newName,
+    });
+    individualProduct.save();
+    // clear the input fields
+    this.newSerialNumber = '';
+    this.newName = '';
+  }
+
+  @action
+  removeIndividualProduct(individualProduct, event) {
+    event.preventDefault();
+    individualProduct.destroyRecord();
+  }
+}
--- app/routes/individual-products.js
+++ app/routes/individual-products.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class IndividualProductsRoute extends Route {}
+export default class IndividualProductsRoute extends Route {
+  @service store;
+
+  model() {
+    return this.store.findAll('individual-product');
+  }
+}
