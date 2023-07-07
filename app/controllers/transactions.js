2746636c02d87378bae3352f27bd6d9656235c19
Peter Nowee <peter@peternowee.com>
2023-07-07 17:00:00 +0200

    Basic setup for transaction page
    This commit only works if the backend considers the values supplied
    for the transaction's person and individual-product as simply strings
    (e.g. in mu-cl-resources: `:properties` of the transaction), not having
    any relationship with resources person and individual-product yet.
    
    Later commits will add relationships to the frontend to make it work
    with a backend that has relationships defined.

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -1,3 +1,34 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
-export default class TransactionsController extends Controller {}
+export default class TransactionsController extends Controller {
+  @tracked newDatetime = '';
+  @tracked newPerson = '';
+  @tracked newIndividualProduct = '';
+
+  @service store;
+
+  @action
+  createTransaction(event) {
+    event.preventDefault();
+    // create the new transaction
+    const transaction = this.store.createRecord('transaction', {
+      datetime: new Date(this.newDatetime),
+      person: this.newPerson,
+      individualProduct: this.newIndividualProduct,
+    });
+    transaction.save();
+    // clear the input fields
+    this.newDatetime = '';
+    this.newPerson = '';
+    this.newIndividualProduct = '';
+  }
+
+  @action
+  removeTransaction(transaction, event) {
+    event.preventDefault();
+    transaction.destroyRecord();
+  }
+}
