4eb4c862e9f5f60a5807b65510a91fd54693ebb6
Peter Nowee <peter@peternowee.com>
2023-08-04 16:18:37 +0200

    Add basic display of triple count

--- app/routes/count.js
+++ app/routes/count.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
-export default class CountRoute extends Route {}
+export default class CountRoute extends Route {
+
+  async model() {
+    let response = await fetch('/count');
+    let json = await response.json();
+    return json.results.bindings[0]["callret-0"]["value"];
+  }
+
+}
