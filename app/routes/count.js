e989a736682420b8ead1393c29515348d3648a75
Peter Nowee <peter@peternowee.com>
2023-09-26 15:46:25 +0200

    Formatting by lint:js:fix eslint

--- app/routes/count.js
+++ app/routes/count.js
@@ -2,11 +2,9 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 
 export default class CountRoute extends Route {
-
   async model() {
     let response = await fetch('/count');
     let json = await response.json();
-    return json.results.bindings[0]["triples"]["value"];
+    return json.results.bindings[0]['triples']['value'];
   }
-
 }
