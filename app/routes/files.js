470885f93252308013746e89b96fede69f095f2f
Peter Nowee <peter@peternowee.com>
2023-09-26 17:34:30 +0200

    Basic setup for files page

--- app/routes/files.js
+++ app/routes/files.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class FilesRoute extends Route {}
+export default class FilesRoute extends Route {
+  @service store;
+
+  model() {
+    return this.store.findAll('file', {
+      include: 'download',
+    });
+  }
+}
