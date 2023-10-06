ed7379794e2912b510e26d4d55108585f7400b50
Peter Nowee <peter@peternowee.com>
2023-10-06 14:02:50 +0200

    Let files route refresh after new file upload

--- app/components/pn-file-upload.js
+++ app/components/pn-file-upload.js
@@ -1,11 +1,16 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
 
 export default class PnFileUploadComponent extends Component {
+  @service router;
+
   @action
   async uploadPhoto(file) {
     try {
+      // eslint-disable-next-line no-unused-vars
       const response = await file.upload('/files');
+      this.router.refresh('files');
     } catch (response) {
       console.error(`File upload failed: ${response}`);
     }
