de73aa89a372a13274f4d3f7f7c84bb99d1845b1
Peter Nowee <peter@peternowee.com>
2023-10-06 14:13:45 +0200

    Delete unused boilerplate code for adding files

--- app/controllers/files.js
+++ app/controllers/files.js
@@ -1,24 +1,9 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
 
 export default class FilesController extends Controller {
-  @tracked newName = '';
-
   @service store;
-
-  @action
-  createFile(event) {
-    event.preventDefault();
-    // create the new file
-    const file = this.store.createRecord('file', {
-      name: this.newName,
-    });
-    file.save();
-    // clear the input fields
-    this.newName = '';
-  }
 
   @action
   removeFile(file, event) {
