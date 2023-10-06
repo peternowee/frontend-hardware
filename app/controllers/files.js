724e56be78fc8be8599fd240748d04cbd7e84a52
Peter Nowee <peter@peternowee.com>
2023-10-06 14:14:09 +0200

    WIP: Deleting files
    Actually `file.destroyRecord()` already makes Ember send a JSON:API
    `DELETE /files/:id` request to backend. Provided the backend dispatcher
    routes that request to the file-service, the file-service will delete
    both the file from disk and all related data (for both the virtual file
    and the physical file) from the triplestore.

--- app/controllers/files.js
+++ app/controllers/files.js
@@ -8,7 +8,18 @@ export default class FilesController extends Controller {
   @action
   removeFile(file, event) {
     event.preventDefault();
-    // TODO: DELETE request to file-service.
-    file.destroyRecord();
+    // The database holds records for virtual files and physical files.
+    // Both have an .id, but the virtual file record has an additional
+    // .download.id containing the id of the related physical file.
+    // mu-semtech/file-service API only works for virtual file .id, but
+    // DELETE on that also deletes the physical file and data for both
+    // virtual and physical file.
+    const relatedId = file.download.id;
+    if (relatedId !== null) {
+      file.destroyRecord();
+      // TODO: Delete related record from local store.
+    } else {
+      console.log('Cannot delete by physical file reference ' + file.id);
+    }
   }
 }
